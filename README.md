08b5606ea009fa626ac2511c78ca3fe92f2caab9
Matheus Carino <eu@matheuscarino.com.br>
2015-11-24 07:46:31 -0200

    First part of the script created

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# Firewall Script - Developed only for my lab tests
+
+version: 2015112401
+
+Created by Matheus Carino
+eu@matheuscarino.com.br
+http://www.matheuscarino.com.br
+http://www.github.com/pexessj
---
+++ firewall.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+### BEGIN INIT INFO
+# Provides:		firewall
+# Required-Start:	
+# Required-Stop:	
+# Default-Start:	2 3 4 5
+# Default-Stop:		
+# Short-Description:	Firewall Script developed by Matheus Carino
+### END INIT INFO
+
+## Variables
+
+WAN1="192.168.0.254"
+WAN2=""
+FW="172.16.0.1"
+LAN="172.16.0.0/24"
+PA="1024:65535"
+
+## Rules
+
+case $1 in 
+
+stop)
+
+# Change all policies to ACCEPT.
+
+iptables -P OUTPUT ACCEPT
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+
+# Clean all the rules of the NAT and FILTER tables.
+
+iptables -t nat -F
+iptables -t filter -F
+
+;;
+
+start)
+
+# Change all policies to DROP.
+
+iptables -P OUTPUT DROP
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+
+# Accepts external access by SSH service on port 22 from any source.
+
+iptables -A INPUT -p tcp -s 0/0 -d $WAN1 --dport 22 -j ACCEPT 
+iptables -A OUTPUT -p tcp -s $WAN1 -d 0/0 --dport $PA -j ACCEPT 
+
+;;
+
+restart)
+
+$0 stop
+sleep 0.5
+$0 start
+
+
+;;
+
+*)
+
+echo 'Please on of the following "START | STOP | RESTART"'
+
+;;
+
+esac
